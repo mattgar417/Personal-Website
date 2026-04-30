2d94801ee5951fbf3b5421a38e5ce63aff3f2d28
Matthew Garcia <85887678+mattgar417@users.noreply.github.com>
2026-04-30 16:17:42 -0400

    Update extracurriculars.tsx

--- pages/extracurriculars.tsx
+++ pages/extracurriculars.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { FaChevronLeft, FaExternalLinkAlt, FaUsers } from 'react-icons/fa';
