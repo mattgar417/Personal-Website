80b5f1e205f9b2d5971ae56125a32a28faf31d0e
Matthew Garcia <85887678+mattgar417@users.noreply.github.com>
2026-08-28 13:16:23 -0400

    Update experiences.tsx

--- pages/experiences.tsx
+++ pages/experiences.tsx
@@ -82,24 +82,24 @@ export default function Experiences() {
 
     return (
 		<div style={{ minHeight: '100vh', backgroundColor: '#1E1E1E', display: 'flex', justifyContent: 'center' }}>
+
+		{/* Banner at the Top */}
+        <div className="flex justify-end space-x-8 mt-8 mb-8 w-full border-b border-gray-600 pb-4 px-4 sm:px-8 md:px-16 lg:px-32">
+            {matthewBanner.map((item, index) => (
+                <div
+                    key={index}
+                    className="banner-item text-white bg-[#2B2B2B] px-4 py-2 rounded-full cursor-pointer hover:bg-[#3A3A3A]"
+                    onClick={() => handleItemClick(item.path, undefined, undefined)}
+                >
+                    {item.label}
+                </div>
+            ))}
+        </div>
+
+		{/* Divider */} 
+        <hr className="w-full border-t border-gray-600 mb-16" />
 			
         <div className={`experiences-container ${isVisible ? 'visible' : ''}`}>
-            {/* Banner at the Top */}
-            <div className="flex justify-end space-x-8 mt-8 mb-8 w-full border-b border-gray-600 pb-4 px-4 sm:px-8 md:px-16 lg:px-32">
-            	{matthewBanner.map((item, index) => (
-                	<div
-                    	key={index}
-                    	className="banner-item text-white bg-[#2B2B2B] px-4 py-2 rounded-full cursor-pointer hover:bg-[#3A3A3A]"
-                    	onClick={() => handleItemClick(item.path, undefined, undefined)}
-                	>
-                    	{item.label}
-                	</div>
-            	))}
-        	</div>
-
-			{/* Divider */} 
-            <hr className="w-full border-t border-gray-600 mb-16" />
-			
             <ItemPage Icon={FaBriefcase} title="My Experiences" color="#2B70C9">
                 <div className="experiences-grid">
                     {experiences.map((experience, index) => (
